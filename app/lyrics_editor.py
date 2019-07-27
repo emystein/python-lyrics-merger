666eb537503afffa1bbad15251ef405e10a2c1ab
Emiliano Menéndez <emystein@gmail.com>
2019-07-27 20:30:41 -0300

    LyricsEditor: merge two split lyrics

--- app/lyrics_editor.py
+++ app/lyrics_editor.py
@@ -1,3 +1,9 @@
 class LyricsEditor(object):
 	def split_paragraphs(self, lyrics):
 		return lyrics.split('\n\n')
+
+	def merge_lyrics(self, lyrics1, lyrics2):
+		split1 = self.split_paragraphs(lyrics1)
+		split2 = self.split_paragraphs(lyrics2)
+		# see: https://stackoverflow.com/questions/7946798/interleave-multiple-lists-of-the-same-length-in-python
+		return [val for pair in zip(split1, split2) for val in pair]
--- tests/test_lyrics_editor.py
+++ tests/test_lyrics_editor.py
@@ -7,3 +7,24 @@ def test_split_lyrics_paragraphs():
 	lyrics_editor = LyricsEditor()
 	paragraphs = lyrics_editor.split_paragraphs(lyrics)
 	assert len(paragraphs) == 12
+
+def test_merge_lyrics_with_same_number_of_paragraphs():
+	lyrics1 = 'lyrics 1 first paragraph\n\nlyrics 1 second paragraph'
+	lyrics2 = 'lyrics 2 first paragraph\n\nlyrics 2 second paragraph'
+	lyrics_editor = LyricsEditor()
+	merged_lyrics = lyrics_editor.merge_lyrics(lyrics1, lyrics2)
+	assert merged_lyrics == ['lyrics 1 first paragraph', 'lyrics 2 first paragraph', 'lyrics 1 second paragraph', 'lyrics 2 second paragraph'] 
+
+def test_merge_lyrics_with_first_lyrics_with_2_paragraphs_and_second_lyrics_with_1_paragraph():
+	lyrics1 = 'lyrics 1 first paragraph\n\nlyrics 1 second paragraph'
+	lyrics2 = 'lyrics 2 first paragraph'
+	lyrics_editor = LyricsEditor()
+	merged_lyrics = lyrics_editor.merge_lyrics(lyrics1, lyrics2)
+	assert merged_lyrics == ['lyrics 1 first paragraph', 'lyrics 2 first paragraph']
+
+def test_merge_lyrics_with_first_lyrics_with_1_paragraph_and_second_lyrics_with_2_paragraphs():
+	lyrics1 = 'lyrics 1 first paragraph'
+	lyrics2 = 'lyrics 2 first paragraph\n\nlyrics 2 second paragraph'
+	lyrics_editor = LyricsEditor()
+	merged_lyrics = lyrics_editor.merge_lyrics(lyrics1, lyrics2)
+	assert merged_lyrics == ['lyrics 1 first paragraph', 'lyrics 2 first paragraph']
