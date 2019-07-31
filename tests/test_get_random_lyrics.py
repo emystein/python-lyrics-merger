a2a37cb6e89a5208a2668ad07e87fdf000aa6678
Emiliano Menéndez <emystein@gmail.com>
2019-07-31 09:24:37 -0300

    Renamed dowloader to lyrics_downloader in RandomLyricsDownloader tests

--- tests/test_get_random_lyrics.py
+++ tests/test_get_random_lyrics.py
@@ -6,18 +6,18 @@ from wikia.lyrics_api_adapter import WikiaLyricsApiClient
 
 
 @pytest.fixture
-def downloader():
+def lyrics_downloader():
     return RandomLyricsDownloader(WikiaRandomSongUrlParser(), WikiaLyricsApiClient())
 
 
-def test_get_random_lyrics_from_wikia(downloader):
-    song = downloader.get_random()
+def test_get_random_lyrics_from_wikia(lyrics_downloader):
+    song = lyrics_downloader.get_random()
     assert song.title != SongTitle('', '')
     assert song.lyrics.text != ''
 
 
-def test_get_random_lyrics_by_artist_from_wikia(downloader):
-    song = downloader.get_random_by_artist('Led Zeppelin')
+def test_get_random_lyrics_by_artist_from_wikia(lyrics_downloader):
+    song = lyrics_downloader.get_random_by_artist('Led Zeppelin')
     assert song.title.artist == 'Led Zeppelin'
     assert song.title.title != ''
     assert song.lyrics.text != ''
