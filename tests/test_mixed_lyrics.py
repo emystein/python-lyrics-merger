1e356ed379b67387507fb96782472d421d80d3b8
Emiliano Menéndez <emystein@gmail.com>
2019-08-04 16:41:57 -0300

    Cleanup test_mixed_lyrics.py

--- tests/test_mixed_lyrics.py
+++ tests/test_mixed_lyrics.py
@@ -2,9 +2,17 @@ import pytest
 from lyrics_mixer.lyrics_mixer import ParagraphInterleaveLyricsMix, MixedLyrics
 import tests.song_factory
 
-def test_mixed_lyrics():
-	song1 = tests.song_factory.create_song_stairway_to_heaven()
-	song2 = tests.song_factory.create_song_born_to_be_wild()
+@pytest.fixture
+def song1():
+	return tests.song_factory.create_song_stairway_to_heaven()
+
+
+@pytest.fixture
+def song2():
+	return tests.song_factory.create_song_born_to_be_wild()
+
+
+def test_mixed_lyrics(song1, song2):
 	lyrics_editor = ParagraphInterleaveLyricsMix()
 	expected = lyrics_editor.mix_lyrics(song1, song2)
 
