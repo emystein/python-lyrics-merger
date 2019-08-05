44111789909ac7875c27c724ce51932c152b524a
Emiliano Menéndez <emystein@gmail.com>
2019-08-05 00:17:41 -0300

    Add REST endpoints for mix randomg songs and mix two specific songs

--- rest_api.py
+++ rest_api.py
@@ -1,16 +1,29 @@
 from flask import Flask, escape, request
-from os import environ
 from lyrics_mixer.lyrics_mixer import LyricsMixer, LineInterleaveLyricsMix, ParagraphInterleaveLyricsMix
 from wikia.lyrics_api_client import WikiaLyricsApiClient
+from lyrics_mixer.song import SongTitle
 
 
 app = Flask(__name__)
-# app.run(host = '0.0.0.0', port = environ.get('PORT'))
 
 lyrics_mixer = LyricsMixer(WikiaLyricsApiClient(), LineInterleaveLyricsMix())
 
 
-@app.route('/mix/artists/<artist1>/<artist2>/')
-def mix_artists(artist1, artist2):
+@app.route('/mix/random')
+def mix_two_random_lyrics():
+	mixed = lyrics_mixer.mix_two_random_lyrics()
+	return f'{escape(str(mixed))}'
+
+
+@app.route('/mix/artists/<artist1>/<artist2>')
+def mix_random_lyrics_by_artists(artist1, artist2):
 	mixed = lyrics_mixer.mix_random_lyrics_by_artists(artist1, artist2)
+	return f'{escape(str(mixed))}'
+
+
+@app.route('/mix/songs/<artist1>/<title1>/<artist2>/<title2>')
+def mix_two_specific_lyrics(artist1, title1, artist2, title2):
+	song_title1 = SongTitle(artist1, title1)
+	song_title2 = SongTitle(artist2, title2)
+	mixed =	lyrics_mixer.mix_two_specific_lyrics(song_title1, song_title2)
 	return f'{escape(str(mixed))}'
