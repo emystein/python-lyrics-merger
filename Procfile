e8fcf54612f341896de52abc09f170b6c75c7133
Emiliano Menéndez <emystein@gmail.com>
2019-08-05 21:46:50 -0300

    Reply to mentions bot

--- Procfile
+++ Procfile
@@ -1,4 +1,5 @@
 web: gunicorn rest_api:app
 worker: python periodic_tweet_bot.py
+worker: python reply_to_mentions_bot.py
 
 
---
+++ reply_to_mentions_bot.py
@@ -0,0 +1,54 @@
+import tweepy
+import logging
+from tweeter import create_api
+import time
+
+from lyrics_mixer.lyrics_mixer import LyricsMixer, LineInterleaveLyricsMix
+from wikia.lyrics_api_client import WikiaLyricsApiClient
+from lyrics_mixer.artists_parser import ArtistsParser
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+lyrics_mixer = LyricsMixer(WikiaLyricsApiClient(), LineInterleaveLyricsMix())
+
+def check_mentions(api, keywords, since_id):
+    logger.info("Retrieving mentions")
+    new_since_id = since_id
+    for tweet in tweepy.Cursor(api.mentions_timeline,
+                               since_id=since_id).items():
+        new_since_id = max(tweet.id, new_since_id)
+        if tweet.in_reply_to_status_id is not None:
+            continue
+        if any(keyword in tweet.text.lower() for keyword in keywords):
+            logger.info(f"Answering to {tweet.user.name}")
+
+            mixed_lyrics = get_mixed_lyrics(tweet)
+
+            api.update_status(
+                status=mixed_lyrics,
+                in_reply_to_status_id=tweet.id,
+            )
+    return new_since_id
+
+
+def get_mixed_lyrics(tweet):
+	artists_parser = ArtistsParser()
+	parse_result = artists_parser.parse(tweet.text)
+	lyrics_mixer = LyricsMixer(WikiaLyricsApiClient(), LineInterleaveLyricsMix())
+	mixed_lyrics = lyrics_mixer.mix_random_lyrics_by_artists(parse_result.artists[0], parse_result.artists[1])
+	return str(mixed_lyrics)
+
+
+def main():
+    api = create_api()
+    since_id = 1
+    while True:
+        print(since_id)
+        since_id = check_mentions(api, ["help", "support"], since_id)
+        logger.info("Waiting...")
+        time.sleep(60)
+
+
+if __name__ == "__main__":
+    main()
