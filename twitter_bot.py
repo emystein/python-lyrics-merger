fb1fc3af34da0dc8898399f984c39149f2dc0c1f
Emiliano Menéndez <emystein@gmail.com>
2019-08-08 19:42:23 -0300

    Use schedule.run_pending in tweeter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -40,8 +40,7 @@ schedule.every(2).to(8).hours.do(tweet_random_lyrics)
 
 def main():
     while True:
-        # schedule.run_pending()
-        schedule.run_all()
+        schedule.run_pending()
         time.sleep(1)
 
 
