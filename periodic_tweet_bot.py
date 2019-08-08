c9b776c0cd28810105f8c2035373ac8ebe727c50
Emiliano Menéndez <emystein@gmail.com>
2019-08-07 22:36:07 -0300

    Reuse tweepy api creation from tweeter module

--- periodic_tweet_bot.py
+++ periodic_tweet_bot.py
@@ -1,24 +1,15 @@
 import time
 import sys
-import tweepy
+from tweeter import create_api
 
 from lyrics_mixer.lyrics_mixer import LyricsMixer, LineInterleaveLyricsMix
 from wikia.lyrics_api_client import WikiaLyricsApiClient
 
 
-# set vars in heroku dashboard
-from os import environ
-CONSUMER_KEY = environ['TWITTER_CONSUMER_KEY']
-CONSUMER_SECRET = environ['TWITTER_CONSUMER_SECRET']
-ACCESS_TOKEN = environ['TWITTER_ACCESS_TOKEN']
-ACCESS_TOKEN_SECRET = environ['TWITTER_ACCESS_TOKEN_SECRET']
-
 MAX_TWEET_LENGTH = 280
 PERIOD_IN_HOURS_BETWEEN_TWEETS = 3
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
+api = create_api()
 
 lyrics_mixer = LyricsMixer(WikiaLyricsApiClient(), LineInterleaveLyricsMix())
 
