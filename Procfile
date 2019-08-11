c968e60cfbeb003682eb9efc513f0a0fd37f974c
Emy <emystein@gmail.com>
2019-08-11 14:03:40 -0300

    Move files to twitter directory

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn rest_api:app
-worker: python twitter_job_scheduler.py
+worker: python twitter/job_scheduler.py
--- twitter/job_scheduler.py
+++ twitter/job_scheduler.py
@@ -1,12 +1,12 @@
 import schedule
 import time
 import twitter
-import twitter_jobs
+import jobs
 
 twitter_api = twitter.create_api()
     
-schedule.every().minute.do(twitter_jobs.reply_to_mentions, twitter_api = twitter_api)
-schedule.every(6).hours.do(twitter_jobs.tweet_random_lyrics, twitter_api = twitter_api)
+schedule.every().minute.do(jobs.reply_to_mentions, twitter_api = twitter_api)
+schedule.every(6).hours.do(jobs.tweet_random_lyrics, twitter_api = twitter_api)
     
 def main():
     while True:
