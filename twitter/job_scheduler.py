c9f8dea5dd5459c07deb33b4733786da47b85c0d
Emiliano Menéndez <emystein@gmail.com>
2019-08-16 01:47:08 -0300

    Organized code

--- twitter/job_scheduler.py
+++ twitter/job_scheduler.py
@@ -3,12 +3,19 @@ import time
 import twitter
 import jobs
 from twitter_api_wrapper import TwitterApiWrapper
+from lyrics_mixer.artists_parser import ArtistsParser
+from lyrics_mixer.lyrics_mixer import LyricsMixer, LineInterleaveLyricsMix, EmptyMixedLyrics
+from wikia.lyrics_api_client import WikiaLyricsApiClient
+from jobs import MixLyricsReplyStrategy
 
 twitter_api = twitter.create_api()
 twitter_api_wrapper = TwitterApiWrapper(twitter_api)
-    
-schedule.every().minute.do(jobs.reply_to_mentions, twitter_api = twitter_api_wrapper)
-schedule.every(6).hours.do(jobs.tweet_random_lyrics, twitter_api = twitter_api_wrapper)
+
+lyrics_mixer = LyricsMixer(WikiaLyricsApiClient(), LineInterleaveLyricsMix())
+reply_strategy = MixLyricsReplyStrategy(ArtistsParser(), lyrics_mixer) 
+
+schedule.every().minute.do(jobs.reply_to_mentions, twitter_api = twitter_api_wrapper, reply_strategy = reply_strategy)
+schedule.every(6).hours.do(jobs.tweet_random_lyrics, twitter_api = twitter_api_wrapper, lyrics_mixer = lyrics_mixer)
     
 def main():
     while True:
--- twitter/twitter.py
+++ twitter/twitter.py
@@ -46,3 +46,15 @@ class MentionsReplyCursor(object):
         new_since_id = mentions[-1].id if len(mentions) > 0 else 1
         self.cursor.position = max(self.cursor.position, new_since_id)
         self.cursor.save()
+
+
+class MixLyricsReplyStrategy(object):
+    def __init__(self, input_parser, lyrics_mixer):
+        self.input_parser = input_parser
+        self.lyrics_mixer = lyrics_mixer
+    
+    def get_reply_for(self, tweet):
+        logger.info(f"Mixing lyrics requested by: {tweet.user.name}, using input: '{tweet.text}'")
+        parsed = self.input_parser.parse(tweet.text)
+        mixed_lyrics = self.lyrics_mixer.mix_random_lyrics_by_artists(parsed.artist1, parsed.artist2)
+        return f"@{tweet.user.name} {mixed_lyrics}"
