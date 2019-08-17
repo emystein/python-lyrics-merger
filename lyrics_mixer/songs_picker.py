b4dd0e818cd4bbdfd33f6333d1b884a4dbe8ae10
Emiliano Menéndez <emystein@gmail.com>
2019-08-17 20:17:23 -0300

    Add logging to song pickers

--- lyrics_mixer/songs_picker.py
+++ lyrics_mixer/songs_picker.py
@@ -4,30 +4,28 @@ import logging
 logger = logging.getLogger()
 
 
-class TwoSongsPicker:
+class TwoRandomSongsPicker:
     def __init__(self, lyrics_api_client):
         self.lyrics_api_client = lyrics_api_client
 
-
-class TwoRandomSongsPicker(TwoSongsPicker):
     def pick_two_songs(self):
-        logger.info('Mixing two random lyrics')
+        logger.info('Picking two random songs')
         return self.lyrics_api_client.get_random_songs(2)
         
 
-class TwoRandomSongsByArtistsPicker(TwoSongsPicker):
+class TwoRandomSongsByArtistsPicker:
     def __init__(self, lyrics_api_client, artist1, artist2):
-        self.lyrics_api_client = lyrics_api_client
-        self.artist1, self.artist2 = artist1, artist2
+        self.lyrics_api_client, self.artist1, self.artist2 = lyrics_api_client, artist1, artist2
 
     def pick_two_songs(self):
+        logger.info(f'Picking two random songs by artists: {self.artist1} and {self.artist2}')
         return self.lyrics_api_client.get_random_songs_by_artists([self.artist1, self.artist2])
 
 
-class TwoSpecificSongsPicker(TwoSongsPicker):
+class TwoSpecificSongsPicker:
     def __init__(self, lyrics_api_client, song_title1, song_title2):
-        self.lyrics_api_client = lyrics_api_client
-        self.song_title1, self.song_title2 = song_title1, song_title2
+        self.lyrics_api_client, self.song_title1, self.song_title2 = lyrics_api_client, song_title1, song_title2
 
     def pick_two_songs(self):
+        logger.info(f'Picking songs: {self.song_title1} and {self.song_title2}')
         return self.lyrics_api_client.get_songs([self.song_title1, self.song_title2])
