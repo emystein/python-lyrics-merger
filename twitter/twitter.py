ea84da2125e1c03d02d7edfbc9a77c2313cce645
Emiliano Menéndez <emystein@gmail.com>
2019-08-18 12:15:29 -0300

    Moved TwitterApiWrapper to twitter module

--- twitter/twitter.py
+++ twitter/twitter.py
@@ -8,7 +8,7 @@ MAX_TWEET_LENGTH = 280
 logger = logging.getLogger()
 
 
-def create_api():
+def create_tweepy_api():
     CONSUMER_KEY = environ['TWITTER_CONSUMER_KEY']
     CONSUMER_SECRET = environ['TWITTER_CONSUMER_SECRET']
     ACCESS_TOKEN = environ['TWITTER_ACCESS_TOKEN']
@@ -25,3 +25,42 @@ def create_api():
         raise e
     logger.info("API created")
     return api
+
+
+class TwitterApiWrapper(object):
+    def __init__(self):
+        tweepy_api = create_tweepy_api()
+        self.twitter_api = tweepy_api
+
+    def mentions_since(self, since_id):
+        tweets = tweepy.Cursor(
+            self.twitter_api.mentions_timeline, since_id).items()
+        mentions = filter(self.is_not_reply, tweets)
+        return map(lambda mention: TweetWrapper(self, mention), mentions)
+
+    def is_not_reply(self, tweet):
+        return tweet.in_reply_to_status_id is None
+
+    def update_status(self, tweet):
+        self.twitter_api.update_status(
+            tweet[:twitter.twitter.MAX_TWEET_LENGTH])
+
+    def reply_tweet_with(self, tweet, reply_text):
+        self.twitter_api.update_status(
+            status=reply_text[:twitter.twitter.MAX_TWEET_LENGTH], in_reply_to_status_id=tweet.id)
+
+
+class TweetWrapper(object):
+    def __init__(self, twitter_api_wrapper, tweet):
+        self.twitter_api_wrapper = twitter_api_wrapper
+        self.tweet = tweet
+        self.id = tweet.id
+        self.user = tweet.user
+        self.text = tweet.text
+
+    def reply_using_strategy(self, reply_strategy):
+        reply_text = reply_strategy.get_reply_for(self)
+        self.reply_with(reply_text)
+
+    def reply_with(self, reply_text):
+        self.twitter_api_wrapper.reply_tweet_with(self.tweet, reply_text)
--- twitter_lyrics_mixer/job_scheduler.py
+++ twitter_lyrics_mixer/job_scheduler.py
@@ -5,18 +5,18 @@ import jobs
 from lyrics_mixer.artists_parser import ArtistsParser
 from lyrics_mixer.lyrics_mixer import LyricsMixer, LineInterleaveLyricsMix
 from wikia.lyrics_api_client import WikiaLyricsApiClient
-from twitter.twitter_api_wrapper import TwitterApiWrapper
+from twitter import TwitterApiWrapper
 from reply_strategies import MixLyricsReplyStrategy
 
 logging.basicConfig(level=logging.INFO)
 
-twitter_api_wrapper = TwitterApiWrapper()
+twitter_api = TwitterApiWrapper()
 
 lyrics_mixer = LyricsMixer(WikiaLyricsApiClient(), LineInterleaveLyricsMix())
 reply_strategy = MixLyricsReplyStrategy(ArtistsParser(), lyrics_mixer) 
 
-schedule.every().minute.do(jobs.reply_to_mentions, twitter_api = twitter_api_wrapper, reply_strategy = reply_strategy)
-schedule.every(6).hours.do(jobs.tweet_random_lyrics, twitter_api = twitter_api_wrapper, lyrics_mixer = lyrics_mixer)
+schedule.every().minute.do(jobs.reply_to_mentions, twitter_api = twitter_api, reply_strategy = reply_strategy)
+schedule.every(6).hours.do(jobs.tweet_random_lyrics, twitter_api = twitter_api, lyrics_mixer = lyrics_mixer)
     
 def main():
     while True:
