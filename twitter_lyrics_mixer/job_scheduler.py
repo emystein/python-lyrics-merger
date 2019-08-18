8e716e516c7991e4337c239b25acc1a31f72c7d7
Emiliano Menéndez <emystein@gmail.com>
2019-08-18 19:46:44 -0300

    Run jobs in parallel

--- twitter_lyrics_mixer/job_scheduler.py
+++ twitter_lyrics_mixer/job_scheduler.py
@@ -9,6 +9,11 @@ from wikia.lyrics_api_client import WikiaLyricsApiClient
 from twitter.twitter import TwitterApi, TweetReplyFactory
 from reply_strategies import MixLyricsReplyStrategy
 
+
+def run_threaded(job_func):
+    job_thread = threading.Thread(target=job_func)
+    job_thread.start()
+
 logging.basicConfig(level=logging.INFO)
 
 twitter_api = TwitterApi()
@@ -18,10 +23,6 @@ lyrics_mixer = LyricsMixer(WikiaLyricsApiClient(), LineInterleaveLyricsMix())
 schedule.every().minute.do(run_threaded, jobs.reply_to_mentions, twitter_api = twitter_api, tweet_reply_factory = TweetReplyFactory(ArtistsParser(), MixLyricsReplyStrategy(lyrics_mixer)))
 schedule.every(6).hours.do(run_threaded, jobs.tweet_random_lyrics, twitter_api = twitter_api, lyrics_mixer = lyrics_mixer)
     
-def run_threaded(job_func):
-    job_thread = threading.Thread(target=job_func)
-    job_thread.start()
-
 def main():
     while True:
         schedule.run_pending()
