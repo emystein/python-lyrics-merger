addcd7682d4a62511351fa8643b4a25162b91a45
Emiliano Menéndez <emystein@gmail.com>
2019-08-19 12:04:04 -0300

    Format song pickers tests

--- songs/tests/test_pickers.py
+++ songs/tests/test_pickers.py
@@ -3,6 +3,7 @@ from unittest.mock import Mock
 from songs.tests.fixtures.songs import song1, song2
 from songs.pickers import *
 
+
 def test_two_random_songs_picker(song1, song2):
 	lyrics_api_client = Mock()
 	lyrics_api_client.get_random_songs.return_value = [song1, song2]
@@ -13,6 +14,7 @@ def test_two_random_songs_picker(song1, song2):
 	lyrics_api_client.get_random_songs.assert_called_once_with(2)
 	assert picked_song_1 == song1
 	assert picked_song_2 == song2
+
 
 def test_two_random_songs_by_artists_picker(song1, song2):
 	lyrics_api_client = Mock()
@@ -36,4 +38,3 @@ def test_two_specific_songs_picker(song1, song2):
 	lyrics_api_client.get_songs.assert_called_once_with([song1.title, song2.title])
 	assert picked_song_1 == song1
 	assert picked_song_2 == song2
-
