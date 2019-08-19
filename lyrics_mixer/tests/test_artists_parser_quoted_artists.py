5736b109c58613f80f8b2834505f12c97d6d5dee
Emiliano Menéndez <emystein@gmail.com>
2019-08-19 19:54:16 -0300

    Split artists parser tests in several files

---
+++ lyrics_mixer/tests/test_artists_parser_quoted_artists.py
@@ -0,0 +1,32 @@
+import pytest
+from lyrics_mixer.artists_parser import ArtistsParser
+from songs.model import SongTitle
+
+@pytest.fixture
+def parser():
+    return ArtistsParser()
+
+
+def test_parse_artists_enclosed_by_single_quote(parser):
+    results = parser.parse("mezcla 'Divididos' y 'Las Pelotas'")
+    assert results.artists == ['Divididos', 'Las Pelotas']
+
+
+def test_parse_first_artist_enclosed_by_single_quote_and_second_artist_enclosed_by_double_quote(parser):
+    results = parser.parse("mezcla 'Divididos' y \"Las Pelotas\"")
+    assert results.artists == ['Divididos', 'Las Pelotas']
+
+
+def test_parse_first_artist_enclosed_by_double_quote_and_second_artist_enclosed_by_single_quote(parser):
+    results = parser.parse("mezcla \"Divididos\" y 'Las Pelotas'")
+    assert results.artists == ['Divididos', 'Las Pelotas']
+
+
+def test_parse_artists_enclosed_by_different_quotes_before_and_after(parser):
+    results = parser.parse("mezcla \"Divididos' y 'Las Pelotas\"")
+    assert results.artists == ['Divididos', 'Las Pelotas']
+
+
+def test_parse_artists_enclosed_by_double_quote(parser):
+    results = parser.parse('mezcla "Divididos" y "Las Pelotas"')
+    assert results.artists == ['Divididos', 'Las Pelotas']
