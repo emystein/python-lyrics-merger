f0a36cac7a1516821bdb50413b2e208c3f126e8b
Emiliano Menéndez <emystein@gmail.com>
2019-08-24 09:10:16 -0300

    Minor cleanup

--- lyrics_mixer/dispatcher.py
+++ lyrics_mixer/dispatcher.py
@@ -8,14 +8,14 @@ class Dispatcher(object):
         self.lyrics_mix_strategy = lyrics_mix_strategy
 
     def mix_two_random_lyrics(self):
-        song_lyrics_mixer = RandomLyricsMixer(self.lyrics_api_client, self.lyrics_mix_strategy)
-        return song_lyrics_mixer.mix_lyrics()
+        lyrics_mixer = RandomLyricsMixer(self.lyrics_api_client, self.lyrics_mix_strategy)
+        return lyrics_mixer.mix_lyrics()
 
     def mix_random_lyrics_by_artists(self, artist1, artist2):
-        song_lyrics_mixer = RandomByArtistsLyricsMixer(self.lyrics_api_client, self.lyrics_mix_strategy, artist1, artist2)
-        return song_lyrics_mixer.mix_lyrics()
+        lyrics_mixer = RandomByArtistsLyricsMixer(self.lyrics_api_client, self.lyrics_mix_strategy, artist1, artist2)
+        return lyrics_mixer.mix_lyrics()
 
     def mix_two_specific_lyrics(self, song_title1, song_title2):
-        song_lyrics_mixer = SpecificLyricsMixer(self.lyrics_api_client, self.lyrics_mix_strategy, song_title1, song_title2)
-        return song_lyrics_mixer.mix_lyrics()
+        lyrics_mixer = SpecificLyricsMixer(self.lyrics_api_client, self.lyrics_mix_strategy, song_title1, song_title2)
+        return lyrics_mixer.mix_lyrics()
 
