1f869fb1ac03a3f527c52b00e09aecca6d48875a
Emiliano Menéndez <emystein@gmail.com>
2020-01-05 23:19:03 -0300

    Revert "Replace Dispatcher with LyricsMixer and lyrics mix strategy in REST API"
    This reverts commit c05b846a63d4d55ce9c40584a69f82c637cfb98d.

--- lyrics_mixer/rest_api/rest_api_context.py
+++ lyrics_mixer/rest_api/rest_api_context.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from injector import Module, Injector, singleton
 from flask_injector import FlaskInjector
-from lyrics_mixer.lyrics_mix_strategies import LineInterleaveLyricsMix
+from lyrics_mixer.dispatcher import Dispatcher
+from lyrics_mixer.lyrics_mix_strategies import LineInterleaveLyricsMix, ParagraphInterleaveLyricsMix
 from wikia.lyrics_api_client import WikiaLyricsApiClient
 from lyrics_mixer.rest_api.rest_api import configure_views
 
@@ -11,10 +12,8 @@ class AppModule(Module):
         self.app = app
 
     def configure(self, binder):
-        lyrics_library = WikiaLyricsApiClient()
-        binder.bind(WikiaLyricsApiClient, to=lyrics_library, scope=singleton)
-        lyrics_mix_strategy = LineInterleaveLyricsMix()
-        binder.bind(LineInterleaveLyricsMix, to=lyrics_mix_strategy, scope=singleton)
+        dispatcher = Dispatcher(WikiaLyricsApiClient(), LineInterleaveLyricsMix())
+        binder.bind(Dispatcher, to=dispatcher, scope=singleton)
 
 
 app = Flask(__name__)
--- lyrics_mixer/tests/test_lyrics_mixer_integration.py
+++ lyrics_mixer/tests/test_lyrics_mixer_integration.py
@@ -1,6 +1,6 @@
 import pytest
 from songs.model import SongTitle
-from lyrics_mixer.lyrics_mixers import *
+from lyrics_mixer.dispatcher import Dispatcher
 from lyrics_mixer.mixed_lyrics import EmptyMixedLyrics
 from lyrics_mixer.lyrics_mix_strategies import ParagraphInterleaveLyricsMix
 from wikia.lyrics_api_client import WikiaLyricsApiClient
@@ -8,30 +8,21 @@ from songs.tests.fixtures.song_titles import song_title1, song_title2
 
 
 @pytest.fixture
-def lyrics_library():
-    return WikiaLyricsApiClient()
-
-@pytest.fixture
-def lyrics_mix_strategy():
-    return ParagraphInterleaveLyricsMix()
+def lyrics_mixer():
+    return Dispatcher(WikiaLyricsApiClient(), ParagraphInterleaveLyricsMix())
 
 
-def test_mix_two_random_lyrics(lyrics_library, lyrics_mix_strategy):
-    lyrics_mixer = RandomLyricsMixer(lyrics_library)
-    mixed_lyrics = lyrics_mixer.mix_lyrics(lyrics_mix_strategy)
-
+# TODO mock WikiaLyricsApiClient and ParagraphInterleaveLyricsMix
+def test_mix_two_random_lyrics(lyrics_mixer):
+    mixed_lyrics = lyrics_mixer.mix_two_random_lyrics()
     assert mixed_lyrics != EmptyMixedLyrics() 
 
-def test_mix_random_lyrics_by_artists(lyrics_library, lyrics_mix_strategy):
-    lyrics_mixer = RandomByArtistsLyricsMixer(lyrics_library, 'Led Zeppelin', 'Steppenwolf')
-    mixed_lyrics = lyrics_mixer.mix_lyrics(lyrics_mix_strategy)
-
+def test_mix_random_lyrics_by_artists(lyrics_mixer):
+    mixed_lyrics = lyrics_mixer.mix_random_lyrics_by_artists('Led Zeppelin', 'Steppenwolf')
     assert mixed_lyrics != EmptyMixedLyrics() 
 
 @pytest.mark.usefixtures('song_title1', 'song_title2')
-def test_mix_two_specific_lyrics(lyrics_library, lyrics_mix_strategy, song_title1, song_title2):
-    lyrics_mixer = SpecificLyricsMixer(lyrics_library, song_title1, song_title2)
-    mixed_lyrics = lyrics_mixer.mix_lyrics(lyrics_mix_strategy)
-
+def test_mix_two_specific_lyrics(lyrics_mixer, song_title1, song_title2):
+    mixed_lyrics = lyrics_mixer.mix_two_specific_lyrics(song_title1, song_title2)
     assert mixed_lyrics.title == str(song_title1) + ', ' + str(song_title2) 
     assert mixed_lyrics.text != ''
