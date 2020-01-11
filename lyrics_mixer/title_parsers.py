404fa7ffd8b036cbdcf7a9d6dcc6a5bc2a22c893
Emiliano Menéndez <emystein@gmail.com>
2020-01-11 09:46:10 -0300

    Minor cleanup

--- lyrics_mixer/title_parsers.py
+++ lyrics_mixer/title_parsers.py
@@ -2,13 +2,14 @@ import re
 from songs.model import SongTitle
 from lyrics_mixer.mix_commands import ArtistsMixCommand, SongTitlesMixCommand
 
-prefixes = r"(?:" + ".*mezcl.|.*combin.|.*mix" + r")?\s?"
+prefixes = r"(?:.*mezcl.|.*combin.|.*mix)?\s?"
+connectors = r"(.*)(?:, | y | con | and | with )(.*)"
+
 
 class ArtistsParser:
     def parse(self, text):
         artist = self.optionally_quoted("([^'\"]+)")
-        match = re.match(prefixes +
-                         artist + " (?:y|con|and|with) " + artist, text)
+        match = re.match(prefixes + artist + connector + artist, text)
         return ArtistsMixCommand(artists=list(match.groups()))
 
     def optionally_quoted(self, pattern):
@@ -17,11 +18,10 @@ class ArtistsParser:
 
 
 class SongTitlesParser:
-	def parse(self, text):
-		match = re.match(prefixes + r"(.*)(?:, | y | and )(.*)", text)
-		titles = list(match.groups())
-		if '-' in text:
-			return SongTitlesMixCommand(titles)
-		else:
-			return ArtistsMixCommand(titles)
-
+    def parse(self, text):
+        match = re.match(prefixes + connectors, text)
+        titles = list(match.groups())
+        if '-' in text:
+            return SongTitlesMixCommand(titles)
+        else:
+            return ArtistsMixCommand(titles)
