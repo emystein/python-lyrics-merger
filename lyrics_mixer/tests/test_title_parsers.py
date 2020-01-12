fa9076549e948b87ce1ad14011a4b5f5cbd89f45
Emiliano Menéndez <emystein@gmail.com>
2020-01-11 22:22:17 -0300

    Revert "Minor cleanup"
    This reverts commit 404fa7ffd8b036cbdcf7a9d6dcc6a5bc2a22c893.

--- lyrics_mixer/tests/test_title_parsers.py
+++ lyrics_mixer/tests/test_title_parsers.py
@@ -5,18 +5,22 @@ from songs.tests.fixtures.song_titles import song_title1, song_title2
 
 parser = SongTitlesParser()
 
-separators = [', ', ' y ', ' and ']
-
 
 @pytest.mark.usefixtures('song_title1', 'song_title2')
-@pytest.mark.parametrize('prefix', ['', '@lyricsmixer mezcla '])
-@pytest.mark.parametrize('separator', separators)
-def test_parse_song_titles(song_title1, song_title2, prefix, separator):
-    result = parser.parse(f"{prefix}{song_title1}{separator}{song_title2}")
+@pytest.mark.parametrize('separator', [', ', ' y ', ' and '])
+def test_parse_song_titles(song_title1, song_title2, separator):
+    result = parser.parse(f"{song_title1}{separator}{song_title2}")
     assert result.song_titles == [song_title1, song_title2]
 
 
-@pytest.mark.parametrize('separator', separators)
+@pytest.mark.usefixtures('song_title1', 'song_title2')
+@pytest.mark.parametrize('separator', [', ', ' y ', ' and '])
+def test_parse_song_titles_including_prefix(song_title1, song_title2, separator):
+    result = parser.parse(f"@lyricsmixer mezclá {song_title1}{separator}{song_title2}")
+    assert result.song_titles == [song_title1, song_title2]
+
+
+@pytest.mark.parametrize('separator', [', ', ' y ', ' and '])
 def test_parse_artists_only(separator):
     result = parser.parse(f"Led Zeppelin{separator}Steppenwolf")
     assert result.artists == ['Led Zeppelin', 'Steppenwolf']
--- lyrics_mixer/title_parsers.py
+++ lyrics_mixer/title_parsers.py
@@ -2,14 +2,13 @@ import re
 from songs.model import SongTitle
 from lyrics_mixer.mix_commands import ArtistsMixCommand, SongTitlesMixCommand
 
-prefixes = r"(?:.*mezcl.|.*combin.|.*mix)?\s?"
-connectors = r"(.*)(?:, | y | con | and | with )(.*)"
-
+prefixes = r"(?:" + ".*mezcl.|.*combin.|.*mix" + r")?\s?"
 
 class ArtistsParser:
     def parse(self, text):
         artist = self.optionally_quoted("([^'\"]+)")
-        match = re.match(prefixes + artist + connector + artist, text)
+        match = re.match(prefixes +
+                         artist + " (?:y|con|and|with) " + artist, text)
         return ArtistsMixCommand(artists=list(match.groups()))
 
     def optionally_quoted(self, pattern):
@@ -18,10 +17,11 @@ class ArtistsParser:
 
 
 class SongTitlesParser:
-    def parse(self, text):
-        match = re.match(prefixes + connectors, text)
-        titles = list(match.groups())
-        if '-' in text:
-            return SongTitlesMixCommand(titles)
-        else:
-            return ArtistsMixCommand(titles)
+	def parse(self, text):
+		match = re.match(prefixes + r"(.*)(?:, | y | and )(.*)", text)
+		titles = list(match.groups())
+		if '-' in text:
+			return SongTitlesMixCommand(titles)
+		else:
+			return ArtistsMixCommand(titles)
+
