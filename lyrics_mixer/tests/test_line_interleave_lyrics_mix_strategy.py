3b1f0b4488e638e31e1460efb3843d9e5cfe2375
Emiliano Menéndez <emystein@gmail.com>
2020-04-05 17:22:54 -0300

    Minor cleanup

--- lyrics_mixer/tests/test_line_interleave_lyrics_mix_strategy.py
+++ lyrics_mixer/tests/test_line_interleave_lyrics_mix_strategy.py
@@ -10,49 +10,49 @@ def lyrics_editor():
 
 def test_mix_with_same_number_of_lines(lyrics_editor):
     song1 = Song('artist1', 'title1',
-                 'lyrics 1 first line\nlyrics 1 second line')
+                 'lyrics 1 line 2\nlyrics 1 line 2\n\nlyrics 1 line 3')
     song2 = Song('artist2', 'title2',
-                 'lyrics 2 first line\nlyrics 2 second line')
+                 'lyrics 2 line 2\nlyrics 2 line 2\n\nlyrics 2 line 3')
     mixed_lyrics = lyrics_editor.mix(song1, song2)
     assert mixed_lyrics.lines == [
-        'lyrics 1 first line', 'lyrics 2 first line', 'lyrics 1 second line', 'lyrics 2 second line']
+        'lyrics 1 line 2', 'lyrics 2 line 2', 'lyrics 1 line 2', 'lyrics 2 line 2', '', '', 'lyrics 1 line 3', 'lyrics 2 line 3']
 
 
 def test_mix_with_first_lyrics_with_2_lines_and_second_lyrics_with_1_line(lyrics_editor):
     song1 = Song('artist1', 'title1',
-                 'lyrics 1 first line\nlyrics 1 second line')
-    song2 = Song('artist2', 'title2', 'lyrics 2 first line')
+                 'lyrics 1 line 2\nlyrics 1 line 2')
+    song2 = Song('artist2', 'title2', 'lyrics 2 line 2')
     mixed_lyrics = lyrics_editor.mix(song1, song2)
-    assert mixed_lyrics.lines == ['lyrics 1 first line', 'lyrics 2 first line']
+    assert mixed_lyrics.lines == ['lyrics 1 line 2', 'lyrics 2 line 2']
 
 
 def test_mix_with_first_lyrics_with_1_line_and_second_lyrics_with_2_lines(lyrics_editor):
-    song1 = Song('artist1', 'title1', 'lyrics 1 first line')
+    song1 = Song('artist1', 'title1', 'lyrics 1 line 2')
     song2 = Song('artist2', 'title2',
-                 'lyrics 2 first line\nlyrics 2 second line')
+                 'lyrics 2 line 2\nlyrics 2 line 2')
     mixed_lyrics = lyrics_editor.mix(song1, song2)
-    assert mixed_lyrics.lines == ['lyrics 1 first line', 'lyrics 2 first line']
+    assert mixed_lyrics.lines == ['lyrics 1 line 2', 'lyrics 2 line 2']
 
 
 def test_mix_with_first_paragraph_containing_one_line_and_second_paragraph_containing_two_lines(lyrics_editor):
     song1 = Song('artist1', 'title1',
-                 'lyrics 1 first line\nlyrics 1 second line\n\nlyrics 1 third line')
+                 'lyrics 1 line 2\nlyrics 1 line 2\n\nlyrics 1 line 3')
     song2 = Song('artist2', 'title2',
-                 'lyrics 2 first line\nlyrics 2 second line\n\nlyrics 2 third line')
+                 'lyrics 2 line 2\nlyrics 2 line 2\n\nlyrics 2 line 3')
     mixed_lyrics = lyrics_editor.mix(song1, song2)
-    assert mixed_lyrics.lines == ['lyrics 1 first line', 'lyrics 2 first line', 'lyrics 1 second line',
-                                  'lyrics 2 second line', '', '', 'lyrics 1 third line', 'lyrics 2 third line']
+    assert mixed_lyrics.lines == ['lyrics 1 line 2', 'lyrics 2 line 2', 'lyrics 1 line 2',
+                                  'lyrics 2 line 2', '', '', 'lyrics 1 line 3', 'lyrics 2 line 3']
     assert mixed_lyrics.paragraphs == [
-        'lyrics 1 first line\nlyrics 2 first line\nlyrics 1 second line\nlyrics 2 second line', 'lyrics 1 third line\nlyrics 2 third line']
+        'lyrics 1 line 2\nlyrics 2 line 2\nlyrics 1 line 2\nlyrics 2 line 2', 'lyrics 1 line 3\nlyrics 2 line 3']
 
 
 def test_mix_with_first_paragraph_containing_two_lines_and_second_paragraph_containing_one_line(lyrics_editor):
     song1 = Song('artist1', 'title1',
-                 'lyrics 1 first line\n\nlyrics 1 second line\nlyrics 1 third line')
+                 'lyrics 1 line 2\n\nlyrics 1 line 2\nlyrics 1 line 3')
     song2 = Song('artist2', 'title2',
-                 'lyrics 2 first line\n\nlyrics 2 second line\nlyrics 2 third line')
+                 'lyrics 2 line 2\n\nlyrics 2 line 2\nlyrics 2 line 3')
     mixed_lyrics = lyrics_editor.mix(song1, song2)
-    assert mixed_lyrics.lines == ['lyrics 1 first line', 'lyrics 2 first line', '', '',
-                                  'lyrics 1 second line', 'lyrics 2 second line', 'lyrics 1 third line', 'lyrics 2 third line']
-    assert mixed_lyrics.paragraphs == ['lyrics 1 first line\nlyrics 2 first line',
-                                       'lyrics 1 second line\nlyrics 2 second line\nlyrics 1 third line\nlyrics 2 third line']
+    assert mixed_lyrics.lines == ['lyrics 1 line 2', 'lyrics 2 line 2', '', '',
+                                  'lyrics 1 line 2', 'lyrics 2 line 2', 'lyrics 1 line 3', 'lyrics 2 line 3']
+    assert mixed_lyrics.paragraphs == ['lyrics 1 line 2\nlyrics 2 line 2',
+                                       'lyrics 1 line 2\nlyrics 2 line 2\nlyrics 1 line 3\nlyrics 2 line 3']
--- lyrics_mixer/tests/test_paragraph_interleave_lyrics_mix_strategy.py
+++ lyrics_mixer/tests/test_paragraph_interleave_lyrics_mix_strategy.py
@@ -10,31 +10,31 @@ def lyrics_editor():
 
 def test_mix_with_same_number_of_paragraphs(lyrics_editor):
     song1 = Song('artist1', 'title1',
-                 'lyrics 1 first line\nlyrics 1 second line\n\nlyrics 1 third line')
+                 'lyrics 1 line 1\nlyrics 1 line 2\n\nlyrics 1 line 3')
     song2 = Song('artist2', 'title2',
-                 'lyrics 2 first line\nlyrics 2 second line\n\nlyrics 2 third line')
+                 'lyrics 2 line 1\nlyrics 2 line 2\n\nlyrics 2 line 3')
     mixed_lyrics = lyrics_editor.mix(song1, song2)
-    assert mixed_lyrics.lines == ['lyrics 1 first line', 'lyrics 1 second line',
-                                  'lyrics 2 first line', 'lyrics 2 second line', 'lyrics 1 third line', 'lyrics 2 third line']
-    assert mixed_lyrics.paragraphs == ['lyrics 1 first line\nlyrics 1 second line',
-                                       'lyrics 2 first line\nlyrics 2 second line', 'lyrics 1 third line', 'lyrics 2 third line']
+    assert mixed_lyrics.lines == ['lyrics 1 line 1', 'lyrics 1 line 2',
+                                  'lyrics 2 line 1', 'lyrics 2 line 2', 'lyrics 1 line 3', 'lyrics 2 line 3']
+    assert mixed_lyrics.paragraphs == ['lyrics 1 line 1\nlyrics 1 line 2',
+                                       'lyrics 2 line 1\nlyrics 2 line 2', 'lyrics 1 line 3', 'lyrics 2 line 3']
 
 
 def test_mix_with_first_lyrics_with_2_paragraphs_and_second_lyrics_with_1_paragraph(lyrics_editor):
     song1 = Song('artist1', 'title1',
-                 'lyrics 1 first line\n\nlyrics 1 second line')
-    song2 = Song('artist2', 'title2', 'lyrics 2 first line')
+                 'lyrics 1 line 1\n\nlyrics 1 line 2')
+    song2 = Song('artist2', 'title2', 'lyrics 2 line 1')
     mixed_lyrics = lyrics_editor.mix(song1, song2)
-    assert mixed_lyrics.lines == ['lyrics 1 first line', 'lyrics 2 first line']
+    assert mixed_lyrics.lines == ['lyrics 1 line 1', 'lyrics 2 line 1']
     assert mixed_lyrics.paragraphs == [
-        'lyrics 1 first line', 'lyrics 2 first line']
+        'lyrics 1 line 1', 'lyrics 2 line 1']
 
 
 def test_mix_with_first_lyrics_with_1_paragraph_and_second_lyrics_with_2_paragraphs(lyrics_editor):
-    song1 = Song('artist1', 'title1', 'lyrics 1 first line')
+    song1 = Song('artist1', 'title1', 'lyrics 1 line 1')
     song2 = Song('artist2', 'title2',
-                 'lyrics 2 first line\n\nlyrics 2 second line')
+                 'lyrics 2 line 1\n\nlyrics 2 line 2')
     mixed_lyrics = lyrics_editor.mix(song1, song2)
-    assert mixed_lyrics.lines == ['lyrics 1 first line', 'lyrics 2 first line']
+    assert mixed_lyrics.lines == ['lyrics 1 line 1', 'lyrics 2 line 1']
     assert mixed_lyrics.paragraphs == [
-        'lyrics 1 first line', 'lyrics 2 first line']
+        'lyrics 1 line 1', 'lyrics 2 line 1']
