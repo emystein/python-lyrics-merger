0c9d34317ec946601978de7c4bfdaa1b476a9da1
Emiliano Menéndez <emystein@gmail.com>
2021-08-31 17:54:11 -0300

    Clean-up lyrics mix strategies tests.

--- lyrics_mixer/tests/test_line_interleave_lyrics_mix.py
+++ lyrics_mixer/tests/test_line_interleave_lyrics_mix.py
@@ -5,61 +5,58 @@ from songs.model import SongTitle, Song, Lyrics, Paragraphs
 song_title1 = SongTitle('artist1', 'title1')
 song_title2 = SongTitle('artist2', 'title2')
 
-
-@pytest.fixture
-def lyrics_editor():
-    return LineInterleaveLyricsMix()
+lyrics_mix_strategy = LineInterleaveLyricsMix()
 
 
-def test_mix_with_same_number_of_lines(lyrics_editor):
+def test_mix_with_same_number_of_lines():
     lyrics1 = Lyrics.with_text('lyrics 1 line 2\nlyrics 1 line 2\n\nlyrics 1 line 3\n\n')
     lyrics2 = Lyrics.with_text('lyrics 2 line 1\nlyrics 2 line 2\n\nlyrics 2 line 3\n\n')
 
-    mixed_lyrics = lyrics_editor.mix(lyrics1, lyrics2)
+    mixed_lyrics = lyrics_mix_strategy.mix(lyrics1, lyrics2)
 
     expected_paragraphs = Paragraphs.from_text('lyrics 1 line 2\nlyrics 2 line 1\nlyrics 1 line 2\nlyrics 2 line 2\nlyrics 1 line 3\nlyrics 2 line 3\n\n')
 
     assert mixed_lyrics.paragraphs == expected_paragraphs
 
 
-def test_mix_with_first_lyrics_with_2_lines_and_second_lyrics_with_1_line(lyrics_editor):
+def test_mix_with_first_lyrics_with_2_lines_and_second_lyrics_with_1_line():
     lyrics1 = Lyrics.with_text('lyrics 1 line 1\nlyrics 1 line 2')
     lyrics2 = Lyrics.with_text('lyrics 2 line 1')
 
-    mixed_lyrics = lyrics_editor.mix(lyrics1, lyrics2)
+    mixed_lyrics = lyrics_mix_strategy.mix(lyrics1, lyrics2)
 
     expected_paragraphs = Paragraphs.from_text('lyrics 1 line 1\nlyrics 2 line 1')
 
     assert mixed_lyrics.paragraphs == expected_paragraphs
 
 
-def test_mix_with_first_lyrics_with_1_line_and_second_lyrics_with_2_lines(lyrics_editor):
+def test_mix_with_first_lyrics_with_1_line_and_second_lyrics_with_2_lines():
     lyrics1 = Lyrics.with_text('lyrics 1 line 1')
     lyrics2 = Lyrics.with_text('lyrics 2 line 1\nlyrics 2 line 2')
 
-    mixed_lyrics = lyrics_editor.mix(lyrics1, lyrics2)
+    mixed_lyrics = lyrics_mix_strategy.mix(lyrics1, lyrics2)
 
     expected_paragraphs = Paragraphs.from_text('lyrics 1 line 1\nlyrics 2 line 1')
 
     assert mixed_lyrics.paragraphs == expected_paragraphs
 
 
-def test_mix_with_first_paragraph_containing_two_lines_and_second_paragraph_containing_one_line(lyrics_editor):
+def test_mix_with_first_paragraph_containing_two_lines_and_second_paragraph_containing_one_line():
     lyrics1 = Lyrics.with_text('lyrics 1 line 1\nlyrics 1 line 2\n\nlyrics 1 line 3')
     lyrics2 = Lyrics.with_text('lyrics 2 line 1\nlyrics 2 line 2\n\nlyrics 2 line 3')
 
-    mixed_lyrics = lyrics_editor.mix(lyrics1, lyrics2)
+    mixed_lyrics = lyrics_mix_strategy.mix(lyrics1, lyrics2)
 
     expected_paragraphs = Paragraphs.from_text('lyrics 1 line 1\nlyrics 2 line 1\nlyrics 1 line 2\nlyrics 2 line 2\nlyrics 1 line 3\nlyrics 2 line 3\n\n')
 
     assert mixed_lyrics.paragraphs == expected_paragraphs
 
 
-def test_mix_with_first_paragraph_containing_one_line_and_second_paragraph_containing_two_lines(lyrics_editor):
+def test_mix_with_first_paragraph_containing_one_line_and_second_paragraph_containing_two_lines():
     lyrics1 = Lyrics.with_text('lyrics 1 line 1\n\nlyrics 1 line 2\nlyrics 1 line 3')
     lyrics2 = Lyrics.with_text('lyrics 2 line 1\n\nlyrics 2 line 2\nlyrics 2 line 3')
 
-    mixed_lyrics = lyrics_editor.mix(lyrics1, lyrics2)
+    mixed_lyrics = lyrics_mix_strategy.mix(lyrics1, lyrics2)
 
     expected_paragraphs = Paragraphs.from_text('lyrics 1 line 1\nlyrics 2 line 1\nlyrics 1 line 2\nlyrics 2 line 2\nlyrics 1 line 3\nlyrics 2 line 3\n\n')
 
--- lyrics_mixer/tests/test_paragraph_interleave_lyrics_mix.py
+++ lyrics_mixer/tests/test_paragraph_interleave_lyrics_mix.py
@@ -1,4 +1,3 @@
-import pytest
 from songs.model import SongTitle, Lyrics, Paragraphs
 from lyrics_mixer.lyrics_mix_strategies import ParagraphInterleaveLyricsMix
 from songs.model import SongTitle, Song, Lyrics, Paragraphs
@@ -6,39 +5,36 @@ from songs.model import SongTitle, Song, Lyrics, Paragraphs
 song_title1 = SongTitle('artist1', 'title1')
 song_title2 = SongTitle('artist2', 'title2')
 
-
-@pytest.fixture
-def lyrics_editor():
-    return ParagraphInterleaveLyricsMix()
+lyrics_mix_strategy = ParagraphInterleaveLyricsMix()
 
 
-def test_mix_with_same_number_of_paragraphs(lyrics_editor):
+def test_mix_with_same_number_of_paragraphs():
     lyrics1 = Lyrics.with_text('lyrics 1 line 1\nlyrics 1 line 2\n\nlyrics 1 line 3')
     lyrics2 = Lyrics.with_text('lyrics 2 line 1\nlyrics 2 line 2\n\nlyrics 2 line 3')
 
-    mixed_lyrics = lyrics_editor.mix(lyrics1, lyrics2)
+    mixed_lyrics = lyrics_mix_strategy.mix(lyrics1, lyrics2)
 
     expected_paragraphs = Paragraphs.from_text('lyrics 1 line 1\nlyrics 1 line 2\n\nlyrics 2 line 1\nlyrics 2 line 2\n\nlyrics 1 line 3\n\nlyrics 2 line 3\n\n')
 
     assert mixed_lyrics.paragraphs == expected_paragraphs
 
 
-def test_mix_with_first_lyrics_with_2_paragraphs_and_second_lyrics_with_1_paragraph(lyrics_editor):
+def test_mix_with_first_lyrics_with_2_paragraphs_and_second_lyrics_with_1_paragraph():
     lyrics1 = Lyrics.with_text('lyrics 1 line 1\n\nlyrics 1 line 2')
     lyrics2 = Lyrics.with_text('lyrics 2 line 1')
 
-    mixed_lyrics = lyrics_editor.mix(lyrics1, lyrics2)
+    mixed_lyrics = lyrics_mix_strategy.mix(lyrics1, lyrics2)
 
     expected_paragraphs = Paragraphs.from_text('lyrics 1 line 1\n\nlyrics 2 line 1\n\n')
 
     assert mixed_lyrics.paragraphs == expected_paragraphs
 
 
-def test_mix_with_first_lyrics_with_1_paragraph_and_second_lyrics_with_2_paragraphs(lyrics_editor):
+def test_mix_with_first_lyrics_with_1_paragraph_and_second_lyrics_with_2_paragraphs():
     lyrics1 = Lyrics.with_text('lyrics 1 line 1')
     lyrics2 = Lyrics.with_text('lyrics 2 line 1\n\nlyrics 2 line 2')
 
-    mixed_lyrics = lyrics_editor.mix(lyrics1, lyrics2)
+    mixed_lyrics = lyrics_mix_strategy.mix(lyrics1, lyrics2)
 
     expected_paragraphs = Paragraphs.from_text('lyrics 1 line 1\n\nlyrics 2 line 1\n\n')
 
